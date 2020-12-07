7833e610cfa07bd594fc711d9fff9f354bb11798
GokoshiJr <julioagonzalez18@gmail.com>
2020-12-07 09:38:22 -0400

    feat: Create basic proyect structure
    Creacion de la estructura basica del proyecto,
    Main y Utilitario.

---
+++ src/evaluacion1/Main.java
@@ -0,0 +1,16 @@
+package evaluacion1;
+
+/**
+ *
+ * @author GokoshiJr
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Utilitario.menu();
+        
+    }
+    
+}
---
+++ src/evaluacion1/Utilitario.java
@@ -0,0 +1,10 @@
+package evaluacion1;
+
+public class Utilitario {
+    
+    public static void menu() {
+        
+        System.out.println("Menu Evaluación 1");
+        
+    }
+}
